19881568247a079cdd43314e7d27c6ee94cd03b6
AlouiOmar97 <omar.aloui@esprit.tn>
2024-10-11 11:52:15 +0100

    crud + form

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Author;
+use App\Form\AddEditAuthorType;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -50,12 +52,55 @@ class AuthorController extends AbstractController
 
     }
 
+    #[Route('/author/new', name: 'app_author_new')]
+    public function newAuthor(Request $request,EntityManagerInterface $em){
+        $author= new Author();
+        //$author->setUsername('Ali');
+        $form= $this->createForm(AddEditAuthorType::class, $author);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em->persist($author);
+            $em->flush();
+            return $this->redirectToRoute('app_author_list');
+        }
+        return $this->render('author/form.html.twig',[
+            'title' => 'Add Author',
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/author/edit/{id}', name: 'app_author_edit')]
+    public function editAuthor($id, AuthorRepository $authorRepository, Request $request,EntityManagerInterface $em){
+        $author= $authorRepository->find($id);
+        $form= $this->createForm(AddEditAuthorType::class, $author);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            //$em->persist($author);
+            $em->flush();
+            return $this->redirectToRoute('app_author_list');
+        }
+        return $this->render('author/form.html.twig',[
+            'title' => 'Update Author',
+            'form' => $form
+        ]);
+    }
+
     #[Route('/author/delete/{id}', name: 'app_author_delete')]
     public function deleteAuthor($id, AuthorRepository $authorRepository, EntityManagerInterface $em){
         $author = $authorRepository->find($id);
         $em->remove($author);
         $em->flush();
-        dd('Author deleted');
+        return $this->redirectToRoute('app_author_list');
+        //dd('Author deleted');
+    }
+
+    #[Route('/author/update/{id}', name: 'app_author_update')]
+    public function updateAuthor($id, AuthorRepository $authorRepository, EntityManagerInterface $em){
+        $author= $authorRepository->find($id);
+        $author->setUsername('usernmae updated');
+        //$em->persist($author);
+        $em->flush();
+        dd($author);
     }
 
     #[Route('/author/details/{id}', name: 'app_author_details')]
